154a97fadabe2e1e4978cbfbfae62e619985dc54
pohui0712 <phxx04@gmail.com>
2024-03-23 00:46:46 +0800

    Fetch games with reactQuery

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: '336f36e801e14efb919856ad84529c4b'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "336f36e801e14efb919856ad84529c4b",
+  },
+});
