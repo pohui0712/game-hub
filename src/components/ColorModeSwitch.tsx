ff82e49eb5e9ffb1b2076f416dc23057280916d9
pohui0712 <phxx04@gmail.com>
2024-03-25 23:54:36 +0800

    Dynamic changing colorMode text

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -10,7 +10,9 @@ const ColorModeSwitch = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text whiteSpace="nowrap">Dark Mode</Text>
+      <Text whiteSpace="nowrap">
+        {colorMode === "dark" ? "Dark" : "Light"} Mode
+      </Text>
     </HStack>
   );
 };
